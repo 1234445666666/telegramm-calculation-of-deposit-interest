24112647de40a6b0801200a80e34199a2383a4f9
Your Name <you@example.com>
2025-01-23 13:50:23 +0300

    fix:command

--- scr/main.js
+++ scr/main.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf')
 const { message } = require('telegraf/filters')
 
-const bot = new Telegraf('private_token')
+const bot = new Telegraf('private_token');
 
 bot.start((ctx) => ctx.reply('Здравствуйте!Приветствую вас в моем боте! Здесь вы можете рассчитать проценты по вкладу. Чтобы начать введите /menu'))
 
@@ -19,23 +19,43 @@ bot.command('web', ctx => {
 
 bot.on(message('text'), (ctx) => {
     const text = ctx.message.text;
-    const parse = text.split(' ');
-    if (parse.length !== 3) {
-        ctx.reply('Вы забыли ввести все данные.')
-        return
-    }
-    const sum = text.split(' ')[0];
-    const days = text.split(' ')[1];
-    const rate = text.split(' ')[2];
-    if (isNaN(sum) || isNaN(days) || isNaN(rate)) {
-        ctx.reply('Введите корректные данные.')
-        return
-    }
-    const total = ((sum * (rate / 100)) / 365) * days;
-    const result = sum + total.toFixed(2);
-    ctx.reply(`Ваша сумма вклада после ${days} дней составляет ${result} рублей.`)
+    const parts = text.split(' ');
 
-})
+    // Проверяем, что введены все три значения
+    if (parts.length !== 3){ 
+        let missingFields = [];
+        if (!parts[0]) missingFields.push('сумма');
+        if (!parts[1]) missingFields.push('количество дней');
+        if (!parts[2]) missingFields.push('процентная ставка');
+
+        ctx.reply(`Вы не ввели: ${missingFields.join(', ')}.\nПример: 10000 365 10`);
+        return; 
+    };
+
+    const sum = parseFloat(parts[0]); 
+    const days = parseFloat(parts[1]); 
+    const rate = parseFloat(parts[2]); 
+
+    //Проверка все ли знания являются числами
+    let invalidFields = [];
+    if (isNaN(sum)) invalidFields.push('сумма');
+    if (isNaN(days)) invalidFields.push('количество дней');
+    if (isNaN(rate)) invalidFields.push('процентная ставка');
+
+    if (invalidFields.length > 0) {
+        ctx.reply(`Некорректные данные: ${invalidFields.join(', ')}.\nПример: 10000 365 10`);
+        return; 
+    };
+
+    // Рассчитываем итоговую сумму
+    const total = ((sum * (rate / 100)) / 365) * days;
+    const result = (sum + total).toFixed(2); 
+
+    ctx.reply(`Ваша сумма вклада после ${days} дней составляет ${result} рублей.`);
+});
+
 
 
 bot.launch();
+
+
